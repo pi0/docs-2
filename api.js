1b3a494c1fddec14d9aab453f31d11ac4de0e5e0
Sarah Dayan <dayan.sarah@gmail.com>
2019-02-22 01:41:14 +0100

    Cleanup code

--- api.js
+++ api.js
@@ -10,53 +10,46 @@ const port = process.env.PORT || 4000
 
 app.listen(port)
 
-const getResources = async (directory, { ignoreFiles = [] }) =>
-  await Promise.all(
-    (await fs.readdirAsync(directory))
-      .filter(child => ignoreFiles.indexOf(child) === -1)
-      .map(child =>
-        fs.lstatSync(directory + '/' + child).isDirectory()
-          ? getResources(directory + '/' + child, { ignoreFiles })
-          : directory + '/' + child
-      )
-  )
+  ; (async () => {
+    const rawResources = await utils.getResources('./docs', {
+      ignoreFiles: ['.DS_Store']
+    })
 
-getResources('./docs', {
-  ignoreFiles: ['.DS_Store']
-}).then(resource => {
-  const resources = resource.flat(Infinity).map(resource => ({
-    resource,
-    route: resource
-      .split('/')
-      .map(x => utils.resourceWithoutOrder(utils.filenameWithoutExtension(x)))
-      .join('/')
-  }))
-  app.get('/docs', async (req, res) => {
-    res.setHeader('Content-Type', 'application/json')
-    return res.send(
-      utils.toPayload(
-        Object.assign(
-          {},
-          ...resources.map(resource => ({
-            [resource.route.substring(1).replace(/([\/-])/gm, '_')]: `${
-              req.protocol
-              }://${req.headers.host}${resource.route}`
-          }))
-        )
-      )
-    )
-  })
-  resources.forEach(route => {
-    app.get(route.route, async (req, res) => {
+    const resources = rawResources.flat(Infinity).map(resource => ({
+      resource,
+      route: resource
+        .split('/')
+        .map(x => utils.resourceWithoutOrder(utils.filenameWithoutExtension(x)))
+        .join('/')
+    }))
+
+    app.get('/docs', async ({ protocol, headers }, res) => {
       res.setHeader('Content-Type', 'application/json')
       return res.send(
-        utils.toPayload({
-          data: utils.parseData(
-            await fs.readFileAsync(route.resource, 'utf8'),
-            path.extname(route.resource).replace('.', '')
+        utils.toPayload(
+          Object.assign(
+            {},
+            ...resources.map(({ route }) => ({
+              [route.substring(1).replace(/([\/-])/gm, '_')]: `${protocol}://${
+                headers.host
+                }${route}`
+            }))
           )
-        })
+        )
       )
     })
-  })
-})
+
+    resources.forEach(route => {
+      app.get(route.route, async (req, res) => {
+        res.setHeader('Content-Type', 'application/json')
+        return res.send(
+          utils.toPayload({
+            data: utils.parseData(
+              await fs.readFileAsync(route.resource, 'utf8'),
+              path.extname(route.resource).replace('.', '')
+            )
+          })
+        )
+      })
+    })
+  })()
--- utils.js
+++ utils.js
@@ -1,10 +1,23 @@
+const bluebird = require('bluebird')
+const fs = bluebird.promisifyAll(require('fs'))
 const yaml = require('yamljs')
+
+const getResources = async (directory, { ignoreFiles = [] }) =>
+  await Promise.all(
+    (await fs.readdirAsync(directory))
+      .filter(child => !ignoreFiles.includes(child))
+      .map(child =>
+        fs.lstatSync(`${directory}/${child}`).isDirectory()
+          ? getResources(`${directory}/${child}`, { ignoreFiles })
+          : `${directory}/${child}`
+      )
+  )
 
 module.exports = {
   filenameWithoutExtension: filename =>
     filename
       .split('.')
-      .filter((x, i, arr) => (arr.length > 1 ? i + 1 !== arr.length : true))
+      .filter((x, i, { length }) => (length > 1 ? i + 1 !== length : true))
       .join(),
   resourceWithoutOrder: resource => resource.replace(/^\d*-/gm, ''),
   parseData: (data, type) =>
@@ -13,5 +26,6 @@ module.exports = {
         return yaml.parse(data)
       }
     }[type] || data),
-  toPayload: resource => `${JSON.stringify(resource, null, 2)}\n`
+  toPayload: resource => `${JSON.stringify(resource, null, 2)}\n`,
+  getResources
 }
